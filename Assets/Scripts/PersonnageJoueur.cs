f3d6d1c5490d38694e28572649b000a64dea7da4
Unknown <thex-pieddroit@hotmail.com>
2018-07-17 16:23:55 +0200

    Deplacement personnage

---
+++ Assets/Scripts/PersonnageJoueur.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+
+public class PersonnageJoueur : MonoBehaviour
+{
+#region Variables (public)
+
+	public float m_fPointsDeVie = 0.0f;
+
+	public float m_fVitesse = 0.0f;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	private void Update()
+	{
+		BougerPersonnage();
+	}
+
+	private void BougerPersonnage()
+	{
+		float fHorizontal = Input.GetAxis("Horizontal");
+		float fVertical = Input.GetAxis("Vertical");
+
+		Vector3 tDirection = new Vector3(fHorizontal, 0.0f, fVertical);
+
+		if (tDirection != Vector3.zero)
+		{
+			tDirection.Normalize();
+
+			Vector3 tDeplacement = tDirection * (m_fVitesse * Time.deltaTime);
+			transform.position += tDeplacement;
+
+			transform.forward = tDirection;
+		}
+	}
+}
