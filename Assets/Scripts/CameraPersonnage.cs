89cf47310fdf42fd9a76e6a0618b874889bae48f
Unknown <thex-pieddroit@hotmail.com>
2018-07-18 11:34:30 +0200

    Camera rotation autour du personnage
    Le personnage ne se deplace pas dans l'espace camera

--- Assets/Scripts/CameraPersonnage.cs
+++ Assets/Scripts/CameraPersonnage.cs
@@ -6,6 +6,7 @@ public class CameraPersonnage : MonoBehaviour
 #region Variables (public)
 
 	public PersonnageJoueur m_pTarget = null;
+	public Transform m_pCameraTransform = null;
 
 	public float m_fDistanceDeSuivi = 0.0f;
 
@@ -18,6 +19,11 @@ public class CameraPersonnage : MonoBehaviour
 	#endregion
 
 
+	private void Update()
+	{
+		TournerCamera();
+	}
+
 	private void LateUpdate()
 	{
 		if (m_pTarget != null)
@@ -26,7 +32,34 @@ public class CameraPersonnage : MonoBehaviour
 
 	private void SuivrePersonnage()
 	{
-		Vector3 tNouvellePosition = m_pTarget.transform.position + Vector3.up - (transform.forward * m_fDistanceDeSuivi);
-		transform.position = tNouvellePosition;
+		Vector3 tNouvellePositionPoint = m_pTarget.transform.position + Vector3.up;
+		transform.position = tNouvellePositionPoint;
+
+		Vector3 tNouvellePositionCamera = tNouvellePositionPoint - (m_pCameraTransform.forward * m_fDistanceDeSuivi);
+		m_pCameraTransform.position = tNouvellePositionCamera;
+	}
+
+	private void TournerCamera()
+	{
+		float fSourisX = Input.GetAxis("Mouse X");
+
+		if (fSourisX != 0.0f)
+		{
+			Vector3 tRotation = new Vector3(0.0f, fSourisX, 0.0f);
+			transform.eulerAngles += tRotation;
+		}
+
+
+		float fSourisY = Input.GetAxis("Mouse Y");
+
+		if (fSourisY != 0.0f)
+		{
+			Vector3 tRotation = new Vector3(-fSourisY, 0.0f, 0.0f);
+
+			Vector3 tRotationSiAjoutee = transform.eulerAngles + tRotation;
+
+			if (tRotationSiAjoutee.x <= 55.0f && tRotationSiAjoutee.x >= 0.0f)
+				transform.eulerAngles = tRotationSiAjoutee;
+		}
 	}
 }
