4296ba316d23a8ed865da4e328fb1b76282edd42
Unknown <thex-pieddroit@hotmail.com>
2018-07-18 09:08:07 +0200

    Camera suit personnage

---
+++ Assets/Scripts/CameraPersonnage.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class CameraPersonnage : MonoBehaviour
+{
+#region Variables (public)
+
+	public PersonnageJoueur m_pTarget = null;
+
+	public float m_fDistanceDeSuivi = 0.0f;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	private void LateUpdate()
+	{
+		if (m_pTarget != null)
+			SuivrePersonnage();
+	}
+
+	private void SuivrePersonnage()
+	{
+		Vector3 tNouvellePosition = m_pTarget.transform.position + Vector3.up - (transform.forward * m_fDistanceDeSuivi);
+		transform.position = tNouvellePosition;
+	}
+}
