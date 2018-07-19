1334d692eb0590bc78824a7d689b14184d5ba294
Unknown <thex-pieddroit@hotmail.com>
2018-07-19 15:15:22 +0200

    Creation du menu principal

---
+++ Assets/Scripts/MenuTitre.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTitre : MonoBehaviour
+{
+#region Variables (public)
+
+	public string m_sNomDeLaSceneDeJeu = null;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	public void LancerJeu()
+	{
+		SceneManager.LoadScene(m_sNomDeLaSceneDeJeu);
+	}
+
+	public void QuitterJeu()
+	{
+		Application.Quit();
+	}
+}
